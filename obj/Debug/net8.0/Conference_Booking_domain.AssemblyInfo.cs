9006f50d7a94659bcca5c8117a3ea0a8218a6fb4
Tshepiso371 <malangkoza@gmail.com>
2026-01-29 02:55:26 +0200

    add changes

--- obj/Debug/net8.0/Conference_Booking_domain.AssemblyInfo.cs
+++ obj/Debug/net8.0/Conference_Booking_domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Conference_Booking_domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8a7e0921f0fb76cb288fa289d7d802ef8650fb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2259c55c58d5ee4b334ca5e78b7c20898ce9716b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Conference_Booking_domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Conference_Booking_domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
