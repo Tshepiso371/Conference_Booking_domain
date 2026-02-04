44240ffcad82d533e4e1124f6793bd5a6b81253a
Tshepiso371 <malangkoza@gmail.com>
2026-02-04 08:36:17 +0200

    add API

---
+++ Conference_Booking.API/Program.cs
@@ -0,0 +1,34 @@
+using Conference_Booking_domain.Data;
+using Conference_Booking_domain.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection; 
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Hosting;
+using Conference_Booking_domain.Domain;
+using Conference_Booking_domain.Enums;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Register services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Register application services
+builder.Services.AddSingleton<BookingRepository>();
+builder.Services.AddSingleton<SeedData>();
+
+var app = builder.Build();
+
+// HTTP pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
