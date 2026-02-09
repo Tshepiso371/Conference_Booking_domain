bfad09338c990a865991d5f95e256334067147d9
Tshepiso371 <malangkoza@gmail.com>
2026-02-09 19:08:11 +0200

    add changes

--- BookingDomain/obj/Debug/net8.0/BookingDomain.AssemblyInfo.cs
+++ BookingDomain/obj/Debug/net8.0/BookingDomain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingDomain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d61b0c8a4211c2f2b2e035f2d45081dbdff42d18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2521aac38a49929ed61226e44f5db8d4aef769cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingDomain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingDomain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
